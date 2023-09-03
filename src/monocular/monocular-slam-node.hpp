85853b1e815dd96805516df31e2419df8de3c2a9
Assume-Zhan <assume0701@gmail.com>
2023-09-03 14:40:54 +0800

    Add required libraries in header

--- src/monocular/monocular-slam-node.hpp
+++ src/monocular/monocular-slam-node.hpp
@@ -1,23 +1,42 @@
 #ifndef __MONOCULAR_SLAM_NODE_HPP__
 #define __MONOCULAR_SLAM_NODE_HPP__
 
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
-
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <chrono>
 #include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
 
+/* ORB-SLAM3 Library */
 #include "System.h"
 #include "Frame.h"
 #include "Map.h"
 #include "Tracking.h"
 
+/* ROS2 Message Library */
+#include "sensor_msgs/msg/image.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+
+/* TF2 Library */
+#include "tf2/exceptions.h"
+#include "tf2/convert.h"
+#include "tf2/transform_datatypes.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+
 #include "utility.hpp"
 
-class MonocularSlamNode : public rclcpp::Node
-{
+class MonocularSlamNode : public rclcpp::Node {
+    
 public:
-    MonocularSlamNode(ORB_SLAM3::System* pSLAM);
 
+    MonocularSlamNode(ORB_SLAM3::System* pSLAM);
     ~MonocularSlamNode();
 
 private:
