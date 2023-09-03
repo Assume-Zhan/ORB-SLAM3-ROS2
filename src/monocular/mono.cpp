5704112de661c3076f81ed070b265a94315fb9e5
Assume-Zhan <assume0701@gmail.com>
2023-09-03 14:42:54 +0800

    Modify entry point

--- src/monocular/mono.cpp
+++ src/monocular/mono.cpp
@@ -1,31 +1,23 @@
-#include <iostream>
-#include <algorithm>
-#include <fstream>
-#include <chrono>
-
-#include "rclcpp/rclcpp.hpp"
 #include "monocular-slam-node.hpp"
 
-#include "System.h"
 
+int main(int argc, char **argv) {
+    if(argc < 3) {
 
-int main(int argc, char **argv)
-{
-    if(argc < 3)
-    {
-        std::cerr << "\nUsage: ros2 run orbslam mono path_to_vocabulary path_to_settings" << std::endl;
+        // Not received required files ( vocabulary, camera config )
+        RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "\nUsage: ros2 run orbslam mono path_to_vocabulary path_to_settings \n");
         return 1;
     }
 
     rclcpp::init(argc, argv);
 
-    // malloc error using new.. try shared ptr
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
     bool visualization = true;
+    
     ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::MONOCULAR, visualization);
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "============================ \n");
 
     auto node = std::make_shared<MonocularSlamNode>(&SLAM);
-    std::cout << "============================ " << std::endl;\
+    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "============================ \n");
 
     rclcpp::spin(node);
     rclcpp::shutdown();
